a2dffd0758495b35c9bb878104e32175b805f8cc
Karthick Ramesh Kumar <karthickraster>
2025-07-15 10:25:10 +0530

    First commit

--- src/components/HouseList.jsx
+++ src/components/HouseList.jsx
@@ -9,6 +9,7 @@ const HouseList = () => {
       { id: houses.id, address: "", country: "", price: "" },
     ]);
   };
+
   return (
     <div className="relative overflow-x-auto">
       <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
