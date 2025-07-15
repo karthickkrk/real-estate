43d2dbfc5d8e7fe9620d26b50bde3cd75638be56
Karthick Ramesh Kumar <karthickraster>
2025-07-15 10:29:41 +0530

    unwanted files

--- commit_push.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-set PATH=%PATH%;"C:\Program Files\Git\bin"
-git add .
-git commit -m "Resolve merge conflict"
-git push -u origin main
--- git_commands.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-set PATH=%PATH%;"C:\Program Files\Git\bin"
-git add .
-git commit -m "Initial commit"
-git push -u origin master
